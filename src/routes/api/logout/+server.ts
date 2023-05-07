65bf403bfcfa3e813c6fab871ba916b65ac8cfcd
Demetrio Battaglia <deme3.iot@gmail.com>
2023-05-07 19:12:57 +0200

    Implemented server-side session invalidation

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,14 +1,29 @@
 import type { RequestHandler } from './$types';
 import config from '../config';
+import Session from '$lib/server/models/Session';
+import Student from '$lib/server/models/Student';
 
 export const POST = (async ({ request, cookies }) => {
 	// Must be the same as the one in the sign-in endpoint
 	// especially path & secure
-	cookies.delete('sessionid', {
-		path: config.cookies.path,
-		httpOnly: config.cookies.httpOnly,
-		secure: config.cookies.secure
-	});
+	let sessId = cookies.get('sessionid');
+
+	if (sessId) {
+		let session = await Session.findById(sessId).exec();
+		if (session) {
+			let user = await Student.findById(session.userId).exec();
+			user?.logout(sessId);
+		} else {
+			console.warn('Tried to log out a non-existent session.');
+			console.warn(`Session ID: ${sessId}`);
+			console.warn(`User Agent: ${request.headers.get('User-Agent')}`);
+		}
+		cookies.delete('sessionid', {
+			path: config.cookies.path,
+			httpOnly: config.cookies.httpOnly,
+			secure: config.cookies.secure
+		});
+	}
 
 	let res = new Response(null, {
 		status: 200
